49da73c9ea32d05418fad5539d32108880256ab5
jimao <shihao.gu@perfma.com>
2024-11-14 19:03:55 +0800

    提交

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -12,23 +12,10 @@ export default function Home() {
     (async () => {
       const { Mina, PublicKey, fetchAccount } = await import('o1js');
       const { Counter } = await import('../../contracts/build/src/');
-
-      // 设置连接到 Berkeley 测试网
       const Devnet = Mina.Network(RPC_ADDRESS);
       Mina.setActiveInstance(Devnet);
-
-      // Update this to use the address (public key) for your zkApp account.
-      // To try it out, you can try this address for an example "Add" smart contract that we've deployed to
-      // Testnet B62qnTDEeYtBHBePA4yhCt4TCgDtA4L2CGvK7PirbJyX4pKH8bmtWe5.
       const zkAppAddress = CONTRACT_ADDRESS;
-
       await fetchAccount({ publicKey: zkAppAddress });
-      // This should be removed once the zkAppAddress is updated.
-      if (!zkAppAddress) {
-        console.error(
-          'The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Testnet: B62qnTDEeYtBHBePA4yhCt4TCgDtA4L2CGvK7PirbJyX4pKH8bmtWe5'
-        );
-      }
       const zkApp = new Counter(PublicKey.fromBase58(zkAppAddress));
       const currentNum = await zkApp.num.get();
       console.log(1234, currentNum.toString());
